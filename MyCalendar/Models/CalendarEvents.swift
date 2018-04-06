b7a54cc87d93f4bc3ba12e084eadbc2f2cbc813c
Vladislav Filyakov <vladf@microsoft.com>
2018-04-06 00:50:53 -0700

    - CalendarEvent: new location property - CalendarEvents: random subjects and locations for mock events - AgendaCell: shows location when available

--- MyCalendar/Models/CalendarEvents.swift
+++ MyCalendar/Models/CalendarEvents.swift
@@ -9,15 +9,17 @@
 import Foundation
 
 class CalendarEvent {
-    init(subject: String, start: Date, duration: TimeInterval) {
+    init(subject: String, start: Date, duration: TimeInterval, location: String) {
         self.subject = subject
         self.start = start
         self.duration = duration
+        self.location = location
     }
     
     var subject: String
     var start: Date
     var duration: TimeInterval
+    var location: String
 }
 
 class CalendarEvents {
@@ -38,22 +40,41 @@ class CalendarEvents {
         let maxEventSpacingUnits: UInt32 = 4
         let eventDurationUnitMinutes: UInt32 = 15
         let maxEventDurationUnits: UInt32 = 8
+        
+        let subjects = [
+            "Outlook UX Team meeting to discuss the recent changes in UI",
+            "Chat with the team about new hires",
+            "1:1 with Satya",
+            "Discussion panel about plans for weekend",
+            "Product planning for FY2019"
+        ]
+        let locations = [
+            "",
+            "Building 32",
+            "Lincoln Square",
+            "City Center",
+            "Some tree house",
+            "Peet's Coffee"
+        ]
 
         let today = Calendar.current.startOfDay(for: Date())
         for day in minDay...maxDay {
             let date = today.addingDays(day)
             var start = date.addingTimeInterval(eventStartHour * 60 * 60)
+            
             var dayEvents = [CalendarEvent]()
             for _ in 0..<arc4random_uniform(UInt32(maxEventCountPerDay + 1)) {
-                let subject = "Office UX Team meeting to discuss the recent changes in UI"    //!!!
+                let subject = subjects[Int(arc4random_uniform(UInt32(subjects.count)))]
+                let location = locations[Int(arc4random_uniform(UInt32(locations.count)))]
 
                 start.addTimeInterval(TimeInterval(arc4random_uniform(maxEventSpacingUnits + 1) * eventSpacingUnitMinutes * 60))
                 let duration = TimeInterval((1 + arc4random_uniform(maxEventDurationUnits)) * eventDurationUnitMinutes * 60)
                 
-                dayEvents.append(CalendarEvent(subject: subject, start: start, duration: duration))
+                dayEvents.append(CalendarEvent(subject: subject, start: start, duration: duration, location: location))
                 
                 start.addTimeInterval(duration)
             }
+            
             events[date] = dayEvents.isEmpty ? nil : dayEvents
         }
     }
--- MyCalendar/Views/Agenda/AgendaCell.swift
+++ MyCalendar/Views/Agenda/AgendaCell.swift
@@ -8,18 +8,26 @@
 
 import UIKit
 
+// TODO: Highlight the current event (hapenning right now)
+
 class AgendaCell: UITableViewCell {
     static let identifier = "AgendaCell"
     
     private static let extraVerticalMargin: CGFloat = 7
+    private static let infoContainerSpacing: CGFloat = 7
+    private static let locationContainerSpacing: CGFloat = 3
+    private static let locationIconSize: CGFloat = 16
+    private static let subjectNumberOfLines = 2
     private static let timeContainerSpacing: CGFloat = 4
     private static let timeContainerWidth: CGFloat = 70
     
     private static let durationFont = UIFont.preferredFont(forTextStyle: .caption1)
+    private static let locationFont = UIFont.preferredFont(forTextStyle: .footnote)
     private static let startFont = UIFont.preferredFont(forTextStyle: .caption1)
     private static let subjectFont = UIFont.preferredFont(forTextStyle: .subheadline)
 
     private static let durationTextColor = UIColor(red: 0.65, green: 0.66, blue: 0.67, alpha: 1)
+    private static let locationTextColor = UIColor(red: 0.56, green: 0.56, blue: 0.58, alpha: 1)
     private static let startTextColor = subjectTextColor
     private static let subjectTextColor = UIColor(white: 0.13, alpha: 1)
     
@@ -35,12 +43,27 @@ class AgendaCell: UITableViewCell {
         subjectLabel.text = event.subject
         startLabel.text = CalendarFormatter.timeString(from: event.start)
         durationLabel.text = CalendarFormatter.durationString(from: event.duration)
+        locationLabel.text = event.location
+        locationContainer.isHidden = event.location.isEmpty
     }
     
     private let durationLabel: UILabel = {
         let label = UILabel()
         label.font = AgendaCell.durationFont
         label.textColor = AgendaCell.durationTextColor
+        return label
+    }()
+    private let locationIcon: UIImageView = {
+        let icon = UIImageView(image: UIImage(named: "icon-pin"))
+        icon.tintColor = AgendaCell.locationTextColor
+        icon.widthAnchor.constraint(equalToConstant: AgendaCell.locationIconSize).isActive = true
+        icon.heightAnchor.constraint(equalToConstant: AgendaCell.locationIconSize).isActive = true
+        return icon
+    }()
+    private let locationLabel: UILabel = {
+        let label = UILabel()
+        label.font = AgendaCell.locationFont
+        label.textColor = AgendaCell.locationTextColor
         return label
     }()
     private let startLabel: UILabel = {
@@ -51,26 +74,38 @@ class AgendaCell: UITableViewCell {
     }()
     private let subjectLabel: UILabel = {
         let label = UILabel()
-        label.numberOfLines = 2
+        label.numberOfLines = AgendaCell.subjectNumberOfLines
         label.font = AgendaCell.subjectFont
         label.textColor = AgendaCell.subjectTextColor
         return label
     }()
 
     private lazy var container: UIStackView = {
-        let container = UIStackView(arrangedSubviews: [timeContainer, subjectLabel])
+        let container = UIStackView(arrangedSubviews: [timeContainer, infoContainer])
         container.axis = .horizontal
         container.alignment = .firstBaseline
         container.isLayoutMarginsRelativeArrangement = true
         container.layoutMargins = UIEdgeInsets(top: AgendaCell.extraVerticalMargin, left: 0, bottom: AgendaCell.extraVerticalMargin, right: 0)
         return container
     }()
+    private lazy var infoContainer: UIStackView = {
+        let container = UIStackView(arrangedSubviews: [subjectLabel, locationContainer])
+        container.axis = .vertical
+        container.spacing = AgendaCell.infoContainerSpacing
+        return container
+    }()
+    private lazy var locationContainer: UIStackView = {
+        let container = UIStackView(arrangedSubviews: [locationIcon, locationLabel])
+        container.axis = .horizontal
+        container.spacing = AgendaCell.locationContainerSpacing
+        return container
+    }()
     private lazy var timeContainer: UIStackView = {
-        let timeContainer = UIStackView(arrangedSubviews: [startLabel, durationLabel])
-        timeContainer.axis = .vertical
-        timeContainer.spacing = AgendaCell.timeContainerSpacing
-        timeContainer.widthAnchor.constraint(equalToConstant: AgendaCell.timeContainerWidth).isActive = true
-        return timeContainer
+        let container = UIStackView(arrangedSubviews: [startLabel, durationLabel])
+        container.axis = .vertical
+        container.spacing = AgendaCell.timeContainerSpacing
+        container.widthAnchor.constraint(equalToConstant: AgendaCell.timeContainerWidth).isActive = true
+        return container
     }()
 
     private func initLayout() {
